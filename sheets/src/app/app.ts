8c8f3184b87511acc14ba29976c49e90a026b0eb
Liberia (Kaiberius) Kartier <136324051+PGK16POWERS@users.noreply.github.com>
2026-02-18 21:23:14 +0000

    ts

--- sheets/src/app/app.ts
+++ sheets/src/app/app.ts
@@ -12,45 +12,50 @@ export class App implements OnInit {
 
   ngOnInit() {
     
+    const handle = document.querySelector(".handle") as HTMLElement;
     const sheet = document.querySelector(".sheet") as HTMLElement;
 
     let startY = 0;
     let currentY = 0;
     let isDragging = false;
+    let startTime = 0;
+    let currentTime = 0;
 
-    sheet.addEventListener("pointerdown", (e) => {
+    handle.addEventListener('pointerdown', (e) => {
       isDragging = true;
+      startTime = Date.now();
       startY = e.clientY;
-      sheet.style.transition = "none";
+      sheet.style.transition = 'none';
     });
 
-    window.addEventListener("pointermove", (e) => {
+    window.addEventListener('pointermove', (e) => {
       if (!isDragging) return;
-    
+
       currentY = e.clientY;
       let diff = currentY - startY;
-    
-      if (diff > 0) { // only drag down
-        sheet.style.transform = `translateY(${diff}px)`;
+
+      if (diff > 0) {
+        sheet.style.transform = `translateY(${diff}px)`
       }
     });
 
-    window.addEventListener("pointerup", () => {
+    window.addEventListener('pointerup', () => {
       if (!isDragging) return;
       isDragging = false;
-    
+
+      sheet.style.transition = 'transform 0.3s ease'
+
+      let currentTime = Date.now();
+      let timeDiff = currentTime - startTime;
       let diff = currentY - startY;
-    
-      sheet.style.transition = "transform 0.3s ease";
-    
-      if (diff > 150) {
-        // Close sheet
-        sheet.style.transform = "translateY(300px)";
+      let velo = diff / timeDiff
+
+      if (diff > 150 || velo > 1) {
+        sheet.style.transform = 'translateY(300px)'
       } else {
-        // Snap back
-        sheet.style.transform = "translateY(0)";
+        sheet.style.transform = 'translateY(0px)'
       }
-    });
+    })
 
   }
 }
